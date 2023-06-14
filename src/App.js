16061522116358b973b48df5e1769e56b2a60439
unnati1612 <unnatijain1612@gmail.com>
2023-06-14 12:02:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ContactList from './components/ContactList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <ToastContainer />
+      <h1 className='text-center text-decoration-underline mb-5 mt-3'>Contact Application</h1>
+      <ContactList />
     </div>
   );
 }
